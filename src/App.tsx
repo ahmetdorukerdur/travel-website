637e282979304be11d7e07ad36293fecbef8ce36
Doruk Erdur <Doruk.Erdur@fups.com>
2023-07-06 15:21:44 +0300

    Created project and first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './app.css';
+import NavBar from './Components/NavBar/NavBar';
+import Home from './Components/Home/Home';
+import Popular from './Components/Popular/Popular';
+import SubInfo from './Components/SubInfo/SubInfo';
+import Offers from './Components/Offers/Offers';
+import About from './Components/About/About';
+import Blog from './Components/Blog/Blog';
+
+const App = () => {
+  return (
+    <>
+      <NavBar />
+      {/* <Home />
+      <Popular />
+      <Offers />
+      <About />
+      <Blog />
+      <SubInfo /> */}
+    </>
+  );
+};
+
+export default App;
---
+++ src/Components/SubInfo/SubInfo.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './subinfo.css';
+
+const SubInfo = () => {
+  return (
+    <div>SubInfo</div>
+  );
+};
+
+export default SubInfo;
